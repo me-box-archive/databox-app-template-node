c9e051a361f683008c4c3dd719efb17258633a11
Yousef Amar <yousefamar@gmail.com>
2017-02-20 18:43:56 +0000

    Add library methods for store catalogue interaction and handle tokens and HTTPS

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var databox = require('./lib/databox.js');
 
+databox.listAvailableStores().then((stores) => console.log(stores));
 
-console.log('hi');
+databox.walkStoreCatalogs().then((stores) => console.log(JSON.stringify(stores, null, '\t'))).catch((err) => console.error(err));
+
+databox.mapStoreCatalogs((cat, i) => {
+	console.log('Store ' + i + ':', JSON.stringify(cat, null, '\t'));
+}).catch((err) => console.error(err));
