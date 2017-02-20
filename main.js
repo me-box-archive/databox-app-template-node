093405ad955e90447c886c16d9987e329e356089
Yousef Amar <yousefamar@gmail.com>
2017-02-20 20:58:53 +0000

    Add API functions for connecting, subscribing, and unsubscribing from store notification WebSockets

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ databox.walkStoreCatalogs().then((stores) => console.log(JSON.stringify(stores, 
 databox.mapStoreCatalogs((cat, i) => {
 	console.log('Store ' + i + ':', JSON.stringify(cat, null, '\t'));
 }).catch((err) => console.error(err));
+
+/*
+var notifications = databox.notifications.connect(href)
+
+notifications.on('open', () => {
+	//console.log('Store notificaitons connection open');
+});
+
+notificaitons.on('data', function (storeHostname, dataSourceID, data) {
+	//console.log(storeHostname, dataSourceID, data);
+});
+*/
